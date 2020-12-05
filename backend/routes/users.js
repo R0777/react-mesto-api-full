3b8b996b5e429baaaa909f064679e9a424cd9fad
R0777 <romka256@yandex.ru>
2020-12-05 20:37:23 +0300

    review: fixnew3

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-
+const { celebrate, Joi } = require('celebrate');
+const urlRegex = require('url-regex');
 const {
   getUser, getUsers, getMe, updateUserAvatar, updateUser,
 } = require('../controllers/users');
@@ -8,10 +9,23 @@ router.get('/users', getUsers);
 
 router.get('/users/me', getMe);
 
-router.patch('/users/me/avatar', updateUserAvatar);
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().regex(urlRegex()),
+  }),
+}), updateUserAvatar);
 
-router.patch('/users/me/', updateUser);
+router.patch('/users/me/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().max(50),
+    about: Joi.string().max(50),
+  }),
+}), updateUser);
 
-router.get('/users/:id', getUser);
+router.get('/users/:id', celebrate({
+  body: Joi.object().keys({
+    id: Joi.string().min(20).max(30).required(),
+  }),
+}), getUser);
 
 module.exports = router;
