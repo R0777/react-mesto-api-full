78ea4928ef882235018b0476310144d1dd080080
R0777 <romka256@yandex.ru>
2020-12-05 20:39:13 +0300

    review: fixnew4

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const urlRegex = require('url-regex');
 
 const {
   getCards, createCard, deleteCard, addLike, unLike,
@@ -6,12 +8,29 @@ const {
 
 router.get('/cards', getCards);
 
-router.post('/cards', createCard);
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    link: Joi.string().required().regex(urlRegex()),
+  }),
+}), createCard);
 
-router.delete('/cards/:id', deleteCard);
+router.delete('/cards/:id', celebrate({
+  body: Joi.object().keys({
+    id: Joi.string().required().min(2).max(50),
+  }),
+}), deleteCard);
 
-router.put('/cards/:id/likes', addLike);
+router.put('/cards/:id/likes', celebrate({
+  body: Joi.object().keys({
+    id: Joi.string().required().min(2).max(50),
+  }),
+}), addLike);
 
-router.delete('/cards/:id/likes', unLike);
+router.delete('/cards/:id/likes', celebrate({
+  body: Joi.object().keys({
+    id: Joi.string().required().min(2).max(50),
+  }),
+}), unLike);
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-const urlRegex = require('url-regex');
+
 const {
   getUser, getUsers, getMe, updateUserAvatar, updateUser,
 } = require('../controllers/users');
@@ -9,23 +8,10 @@ router.get('/users', getUsers);
 
 router.get('/users/me', getMe);
 
-router.patch('/users/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().required().regex(urlRegex()),
-  }),
-}), updateUserAvatar);
+router.patch('/users/me/avatar', updateUserAvatar);
 
-router.patch('/users/me/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().max(50),
-    about: Joi.string().max(50),
-  }),
-}), updateUser);
+router.patch('/users/me/', updateUser);
 
-router.get('/users/:id', celebrate({
-  body: Joi.object().keys({
-    id: Joi.string().min(20).max(30).required(),
-  }),
-}), getUser);
+router.get('/users/:id', getUser);
 
 module.exports = router;
