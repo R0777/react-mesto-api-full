c0eae61bd6843556ea2518c78193a5b2adf5b360
R0777 <romka256@yandex.ru>
2020-12-05 20:56:48 +0300

    review: fixnew7

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { errors, celebrate, Joi } = require('celebrate');
-const urlRegex = require('url-regex');
 
 const {
   getCards, createCard, deleteCard, addLike, unLike,
@@ -11,7 +10,7 @@ router.get('/cards', getCards);
 router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required(),
-    link: Joi.string().required().regex(urlRegex),
+    link: Joi.string().required(),
   }),
 }), createCard);
 
