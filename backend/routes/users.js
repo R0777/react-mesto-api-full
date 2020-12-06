126e2a3870ee5847745eccd8d716fc5c081f5b78
R0777 <romka256@yandex.ru>
2020-12-06 23:47:27 +0300

    review: fixnew15

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -17,8 +17,8 @@ router.patch('/users/me/avatar', celebrate({
 
 router.patch('/users/me/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
   }),
 }), updateUser);
 
