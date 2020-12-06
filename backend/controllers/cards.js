53f5064aa30ac971fabb95fd2ce2f640114eb018
R0777 <romka256@yandex.ru>
2020-12-07 00:21:29 +0300

    review: fixnew17

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -15,7 +15,7 @@ const deleteCard = async (req, res, next) => {
   try {
     const { id } = req.params;
     const card = await Card.findOne({ _id: id });
-    if (card) {
+    if (card && id === req.user.id) {
       await Card.deleteOne(card);
       return res.status(200).send({ message: 'Карточка удалена' });
     }
